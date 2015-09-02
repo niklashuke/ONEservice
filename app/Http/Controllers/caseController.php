c2fe5568efb066709fff149ea681953e458b5aba
Niklas Huke <niklas.huke@gmail.com>
2015-09-02 13:54:24 +0200

    init commit
    init

---
+++ app/Http/Controllers/caseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class caseController extends Controller
+{
+    public function about ()
+    {
+    	$name = 'Huke AB';
+    	return view('pages.about')-> with('name', $name);
+    }
+
+}
