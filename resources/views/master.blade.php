f83ad8e831516c0d9a7b61f38045867ebead2779
Niklas Huke <niklas.huke@gmail.com>
2015-09-02 15:10:46 +0200

    master added

---
+++ resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta Charset="UTF8"> 
+        <title>ONEs</title>
+        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    </head>
+    <body>
+         <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+
+    </body>
+</html>
